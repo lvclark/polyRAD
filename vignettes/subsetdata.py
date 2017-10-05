0a8e260c6649d438af0165e22e1a3a92b5ffa2aa
Lindsay Clark <dragonzuela04@gmail.com>
2017-10-05 18:25:46 -0500

    Diversity panel pipelines added to tutorial

---
+++ vignettes/subsetdata.py
@@ -0,0 +1,16 @@
+# make a subset of UNEAK data to keep for tutorial
+nMarker = 200 # number of markers to keep
+
+filePrefix = "HapMapMsiSubset"
+outPrefix = "HapMapMsiSubset1"
+
+with open(filePrefix + ".fas.txt", mode = 'rt') as incon:
+    with open(outPrefix + ".fas.txt", mode = 'wt') as outcon:
+        for i in range(nMarker * 2):
+            outcon.write(incon.readline())
+
+with open(filePrefix + ".hmc.txt", mode = 'rt') as incon:
+    with open(outPrefix + ".hmc.txt", mode = 'wt') as outcon:
+        for i in range(nMarker + 1):
+            outcon.write(incon.readline())
+            
