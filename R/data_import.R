535bbe81e65f9ce22e22a9045d2142e9694fba91
Lindsay V. Clark <lvclark@illinois.edu>
2019-10-18 16:18:15 -0500

    Update header interpretation for CIGAR inclusion

--- R/data_import.R
+++ R/data_import.R
@@ -1249,7 +1249,7 @@ readProcessSamMulti <- function(alignfile, depthfile = sub("align", "depth", ali
                       quiet = TRUE)
   depthheader <- scan(depthcon, sep = ",", nlines = 1, what = character(),
                       quiet = TRUE)
-  nalign <- (length(alignheader) - 1) / 2 # number of alignment positions reported
+  nalign <- (length(alignheader) - 1) / 3 # number of alignment positions reported
   samples <- depthheader[-1]
   nsam <- length(samples)
   
