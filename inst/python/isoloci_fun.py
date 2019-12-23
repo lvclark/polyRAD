20f228fd975cef79452317978629db6295a63633
Lindsay V. Clark <lvclark@illinois.edu>
2019-12-23 16:02:43 -0600

    Recreate longest reference stretch possible

--- inst/python/isoloci_fun.py
+++ inst/python/isoloci_fun.py
@@ -384,7 +384,18 @@ def PadPosition(tagnucs):
 def RecreateReference(tags, cigars, MDs):
   '''Recreate the reference tag from CIGAR and MD information from the SAM
   file.'''
-  # First, see if there are any tags that are already known to be the reference
+  # Determine the length of the reference spanned by each tag, and pick the longest.
+  cigsplits = [SplitCigar(cig) for cig in cigars]
+  cigsplits = [[s for s in cs if s[-1] not in set('ISHP')] for cs in cigsplits] # remove insertions
+  align_lengths = [sum([int(s[:-1]) for s in cs]) for cs in cigsplits] # get total
+  longest = max(align_lengths)
+  keep = [i for i in range(len(tags)) if align_lengths[i] == longest]
+  
+  tags = [tags[i] for i in keep]
+  cigars = [cigars[i] for i in keep]
+  MDs = [MDs[i] for i in keep]
+  
+  # See if there are any tags that are already known to be the reference
   refs = [i for i in range(len(tags)) if re.match("\d+M$", cigars[i]) != None and \
   re.match("\d$", MDs[i]) != None]
 
