6972657a1e42d30b38a6eb16252dc8c5603ae95b
Lindsay Clark <lvclark@illinois.edu>
2020-06-26 17:32:14 -0500

    fix dirichlet formula

--- R/simulation.R
+++ R/simulation.R
@@ -17,11 +17,10 @@ sampleGenotype <- function(freq, inbreeding, ploidy){
 }
 
 # Randomly generate reads based on a genotype and overdispersion
-sampleReads <- function(geno, nreads, overdispersion = 9){
+sampleReads <- function(geno, nreads, overdispersion = 20){
   initprobs <- geno / sum(geno)
   alpha <- initprobs * overdispersion
-  beta <- overdispersion - alpha
-  newprobs <- rgamma(length(geno), alpha, beta)
+  newprobs <- rgamma(length(geno), alpha, 1)
   
   reads <- sample.int(length(geno), size = nreads, replace = TRUE, prob = newprobs)
   out <- sapply(1:length(geno), function(x) sum(reads == x))
@@ -32,11 +31,8 @@ sampleReads <- function(geno, nreads, overdispersion = 9){
 # sampleReads(c(1, 2, 1), 30, overdispersion = 100)
 # 
 # testprob <- c(0.5, 0.25, 0.25)
-# overdispersion <- 9
-# a <- testprob * overdispersion
-# b <- overdispersion * (1 - testprob)
 # 
-# test1 <- t(sapply(1:1000, function(x) sampleReads(testprob * 4, 30, overdispersion = 100)))
+# test1 <- t(sapply(1:1000, function(x) sampleReads(testprob * 4, 30, overdispersion = 20)))
 # test2 <- MultiRNG::draw.multinomial(1000, 3, testprob, 30)
 # 
 # median(apply(test1, 1, dmultinom, prob = testprob))
