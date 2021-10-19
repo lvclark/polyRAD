a848a37fc9960a9b80753adf9ebcb2315b83d941
Lindsay Clark <lvclark@illinois.edu>
2021-10-19 14:58:11 -0500

    Update TestOverdispersion for multiploidy

--- R/overdispersion.R
+++ R/overdispersion.R
@@ -20,59 +20,68 @@ TestOverdispersion.RADdata <- function(object, to_test = seq(6, 20, by = 2),
   pldindex <- as.integer(names(bpt)[which.max(bpt)])[1]
   theseal <- which(bp == pldindex)
   # identify genotypes that probably have one allele copy
-  one_copy_gen <- which(object$posteriorProb[[pldindex]][2,,theseal] >= 0.95)
-  if(length(one_copy_gen) == 0){
+  one_copy_gen <-
+    sapply(object$posteriorProb[pldindex,],
+           function(x){
+             which(x[2,,theseal] >= 0.95)
+           }, simplify = FALSE)
+  if(all(lengths(one_copy_gen) == 0)){
     stop("Genotype quality too low for this protocol.")
   }
   # get rough probability of sampling allele from this gen (1/ploidy)
-  samprob <- 1 / sum(object$priorProbPloidies[[pldindex]])
+  samprobs <- 1 / sum(object$priorProbPloidies[[pldindex]]) /
+    as.integer(colnames(object$priorProb)) * 2
   
-  # get allele counts for these genotypes
-  alcnt <- object$alleleDepth[,theseal][one_copy_gen]
-  # get total read counts for these genotypes
-  totcnt <- alcnt + object$antiAlleleDepth[,theseal][one_copy_gen]
-  # set up values where we need distribution fn
-  testcnt <- mapply(function(n, k){
-    expected <- n * samprob
-    if(k == expected){
-      return(0:n)
-    } else {
-      if(k > expected){
-        hi <- k
-        lo <- floor(2 * expected - k)
-        out <- hi:n
-        if(lo >= 0){
-          out <- c(0:lo, out)
-        }
-      } else {
-        lo <- k
-        hi <- ceiling(2 * expected - k)
-        out <- 0:lo
-        if(hi >= lo + 1){
-          out <- c(out, hi:n)
-        }
-      }
-      return(out)
-    }
-  }, totcnt, alcnt)
-  # set up sampling permutations (except for alcnt == totcnt/2)
-  perm <- mapply(lchoose, totcnt, testcnt)
-
   # set up list for p-value output
-  outlist <- list()
-  length(outlist) <- length(to_test)
+  outlist <- list(numeric(0))[rep(1, length(to_test))]
   names(outlist) <- as.character(to_test)
   
-  # loop through possible overdispersion parameters
-  for(i in 1:length(to_test)){
-    op <- to_test[i] * samprob # overdispersion parameter times sampling prob.
-    np <- to_test[i] * (1 - samprob)
-    outlist[[i]] <- rep(NA_real_, length(totcnt)) # vector for p-values
-    for(g in 1:length(totcnt)){ # loop through genotypes
-      outlist[[i]][g] <- sum(exp(perm[[g]] +
-                                   lbeta(testcnt[[g]] + op, 
-                                         totcnt[g] - testcnt[[g]] + np) - 
-                                   lbeta(op, np)))
+  for(h in seq_len(ncol(object$priorProb))){
+    # samples with this ploidy
+    thesesam <- dimnames(object$posteriorProb[[pldindex,h]])[[2]]
+    # get allele counts for these genotypes
+    alcnt <- object$alleleDepth[thesesam,theseal][one_copy_gen[[h]]]
+    # get total read counts for these genotypes
+    totcnt <- alcnt + object$antiAlleleDepth[thesesam,theseal][one_copy_gen[[h]]]
+    # set up values where we need distribution fn
+    testcnt <- mapply(function(n, k){
+      expected <- n * samprobs[h]
+      if(k == expected){
+        return(0:n)
+      } else {
+        if(k > expected){
+          hi <- k
+          lo <- floor(2 * expected - k)
+          out <- hi:n
+          if(lo >= 0){
+            out <- c(0:lo, out)
+          }
+        } else {
+          lo <- k
+          hi <- ceiling(2 * expected - k)
+          out <- 0:lo
+          if(hi >= lo + 1){
+            out <- c(out, hi:n)
+          }
+        }
+        return(out)
+      }
+    }, totcnt, alcnt)
+    # set up sampling permutations (except for alcnt == totcnt/2)
+    perm <- mapply(lchoose, totcnt, testcnt)
+    
+    # loop through possible overdispersion parameters
+    for(i in 1:length(to_test)){
+      op <- to_test[i] * samprobs[h] # overdispersion parameter times sampling prob.
+      np <- to_test[i] * (1 - samprobs[h])
+      temp <- rep(NA_real_, length(totcnt)) # vector for p-values
+      for(g in 1:length(totcnt)){ # loop through genotypes
+        temp[g] <- sum(exp(perm[[g]] +
+                             lbeta(testcnt[[g]] + op, 
+                                   totcnt[g] - testcnt[[g]] + np) - 
+                             lbeta(op, np)))
+      }
+      outlist[[i]] <- c(outlist[[i]], temp)
     }
   }
   
